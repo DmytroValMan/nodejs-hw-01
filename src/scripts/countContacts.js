e13d646f4ec86947e43c3597b00c3eb0bd195d0a
DmytroValMan <dmitri.m2011@gmail.com>
2025-06-24 21:49:43 +0300

    node hw-1

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,3 +1,12 @@
-export const countContacts = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+
+export const countContacts = async () => {
+  try {
+    const contactsArray = await readContacts();
+    return contactsArray.length;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 console.log(await countContacts());
